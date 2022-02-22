e0277eb2170431a6b5c80389d96402daf01fc20b
Michael Oppong <michael.oppong@xennialinnovations.com>
2022-02-22 13:28:11 -0500

    adding button

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fastlane testing</Text>
       <StatusBar style="auto" />
+      <Button title='Click me'/>
     </View>
   );
 }
